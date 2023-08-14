d724bc82a181b21909e890fc8c7addc354ca18e1
N. A. Ronald <nronald@swin.edu.au>
2023-08-15 07:28:59 +1000

    First commit.

---
+++ app/src/main/java/au/edu/swin/sdmd/w07_numberlist_2023/MainActivity.kt
@@ -0,0 +1,16 @@
+package au.edu.swin.sdmd.w07_numberlist_2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val numberList = findViewById<RecyclerView>(R.id.numberList)
+
+        val data = IntArray(100) { it }.filter { it % 3 == 0 || it % 5 == 0}
+    }
+}
