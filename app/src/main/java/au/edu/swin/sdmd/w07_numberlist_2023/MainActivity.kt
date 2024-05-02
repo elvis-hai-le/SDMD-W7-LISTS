4d12077e72b927c032bcb96900f895badb18ecd5
Elvis Le <elvishaile120@gmail.com>
2024-05-03 01:27:31 +1000

    - Added menu options to only show multiples of 3 or 5. - Refactored code so list could be redrawn on menu item selected. - Added menu xml file. - Updated themes to display toolbar as options menu was not visible otherwise.

--- app/src/main/java/au/edu/swin/sdmd/w07_numberlist_2023/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w07_numberlist_2023/MainActivity.kt
@@ -5,17 +5,48 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    var data = IntArray(100) { it }.filter { it % 3 == 0 || it % 5 == 0}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        updateList()
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem):Boolean {
+        return when (item.itemId) {
+            R.id.multi3 -> {
+                data = IntArray(100) { it }.filter { it % 3 == 0 }
+                updateList()
+                true
+            }
+            R.id.multi5 -> {
+                data = IntArray(100) { it }.filter { it % 5 == 0 }
+                updateList()
+                true
+            }
+            R.id.resetMenu -> {
+                data = IntArray(100) { it }.filter { it % 3 == 0 || it % 5 == 0}
+                updateList()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun updateList() {
         val numberList = findViewById<RecyclerView>(R.id.numberList)
-
-        val data = IntArray(100) { it }.filter { it % 3 == 0 || it % 5 == 0}
 
         numberList.adapter = NumberAdapter(data) { showDetail(it)}
         numberList.layoutManager = LinearLayoutManager(this)
@@ -27,8 +58,5 @@ class MainActivity : AppCompatActivity() {
         startActivity(i)
     }
 
-//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        val inflater: MenuInflater = menuInflater
-//        inflater.inflate(R.menu.game)
-//    }
+
 }
